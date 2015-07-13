0806fa7d30d44458505edbca8253cb9fadfc6c84
grexor <gregor@imlslnx-gaia.uzh.ch>
2015-07-13 13:34:52 +0200

    Cleaned config file.

--- config/__init__.py
+++ config/__init__.py
@@ -1,10 +1,7 @@
-# PATHS (all paths are relative to apa root folder
+# PATHS (all paths are relative to apa root_folder (see apa.path)
 
 data_folder = "data.apa"
 polya_folder = "data.polya"
 comps_folder = "data.comps"
 iCLIP_folder = "data.iCLIP"
-process = True # process all species
-process = ["hg19", "mm10", "rn5", "dm5"] # limit to specific species
-#process = ["hg19", "mm10"] # limit to specific species
-#process = ["rn5"] # limit to specific species
+process = True # process all species, or list of genome assemblies, eg. ["mm10", "hg19"]
