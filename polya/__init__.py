2ad82d904dc0cecf51b6b78d35ae429d69c3431a
grexor <gregor@imlslnx-gaia.uzh.ch>
2015-07-13 17:28:46 +0200

    Polydb site classification with polyar.

--- polya/__init__.py
+++ polya/__init__.py
@@ -24,9 +24,8 @@ def get_species(poly_id):
         return "hg19"
     if poly_id in pybio.genomes.genomes_list():
         species = poly_id
-    else:
+    else: # ? poly_id is a pool of experiments? read config file
         species = set()
-        # is the poly_id a list of experiments (=pool) filename?
         f = open(os.path.join(apa.path.polya_folder, "%s.config" % poly_id), "rt")
         r = f.readline()
         while r:
@@ -48,9 +47,9 @@ def process(poly_id):
     Also, each experiment is identified with an id (filename). All this is done by pybio.data.Bedgraph.load()
     cpm: counts per milion: when scaled, each experiment position if tested: scale vaue >= cpm, and file id is added to the "support" database accordingly
     After all T files are combined and read into the master bed file, clustering is performed on raw data, scale data, support data and meta data.
-    
+
     bed.save(polyadb_filename+".%02gmincDNA.%03gcpm" % (min_cDNA, cpm), db_filter="support", db_save="raw", min_cDNA=min_cDNA)
-    
+
     This saves all positions where the minimal number of experiments that contributed to the position >= min_cDNA. This is the filtering, but
     raw data (sum of all experiments) is saved.
     """
@@ -76,7 +75,7 @@ def process(poly_id):
             map_to = apa.annotation.libs[lib_id].experiments[exp_id]["map_to"]
             r = f.readline()
         f.close()
-    
+
     bed = pybio.data.Bedgraph()
     num_read = 0
     for lib_id, exp_id in experiments:
@@ -107,14 +106,15 @@ def get_gene(species, gid):
         if type(gene["gene_stop"])==list:
             gene["gene_stop"] = gene["gene_stop"][-1]
     return gene
-    
+
 def annotate(poly_id):
     species = get_species(poly_id)
     interval_types = {"3":"3utr", "5": "5utr", "o":"orf", "i":"intron"}
     polyadb_temp = apa.path.polyadb_filename(poly_id, filetype="temp")
     polyadb_tab = apa.path.polyadb_filename(poly_id, filetype="tab")
     polyadb_bed = apa.path.polyadb_filename(poly_id, filetype="bed")
-    
+    polyadb_fasta = apa.path.polyadb_filename(poly_id, filetype="fasta")
+
     db = {}
     db_values = []
     gene_values = {}
@@ -143,9 +143,12 @@ def annotate(poly_id):
             accepted[key] = 1
 
     ftab = open(polyadb_tab, "wt")
-    ftab.write("\t".join(["chr", "strand", "pos", "gene_id", "gene_name", "interval", "cDNA"]) + "\n")
+    ftab.write("\t".join(["chr", "strand", "pos", "gene_id", "gene_name", "interval", "cDNA", "seq_-125_0"]) + "\n")
     fbed = open(polyadb_bed, "wt")
     fbed.write("track type=bedGraph name=\"%s\" description=\"%s\" altColor=\"200,120,59\" color=\"120,101,172\" maxHeightPixels=\"100:50:0\" visibility=\"full\" priority=\"20\"" % (poly_id, poly_id))
+
+    # store upstream sequences for polyar to classify weak/strong/other poly-A sites
+    ffasta = open(polyadb_fasta, "wt")
     f = open(polyadb_temp, "rt")
     r = f.readline()
     r = f.readline()
@@ -155,21 +158,30 @@ def annotate(poly_id):
         pos = int(r[1])
         cDNA = float(r[-1])
         strand = "+" if cDNA>=0 else "-"
+        # get upstream sequence
+        if strand=="+":
+            seq = pybio.genomes.seq(species, chr, strand, pos-125, pos)
+        else:
+            seq = pybio.genomes.seq(species, chr, strand, pos, pos+125)
+        if seq==None:
+            seq = "" # todo: check for which chrs there is no seq
         gid_up, gid, gid_down, gid_interval = annotate_position(species, chr, strand, pos)
         key = "%s:%s:%s" % (chr, strand, pos)
         if accepted.get(key, None)!=None:
             if gid==None:
-                row = [chr, strand, pos, "", "", "", cDNA]
+                row = [chr, strand, pos, "", "", "", cDNA, seq]
             else:
                 gene = get_gene(species, gid)
                 interval = "%s:%s:%s" % (str(gid_interval[0]), str(gid_interval[1]), interval_types[gid_interval[2]])
-                row = [chr, strand, pos, gid, gene["gene_name"], interval, cDNA]
+                row = [chr, strand, pos, gid, gene["gene_name"], interval, cDNA, seq]
             ftab.write("\t".join(str(e) for e in row)+"\n")
             fbed.write("\t".join(r)+"\n")
+            ffasta.write(">%s\n%s\n" % ("%s%s:%s" % (strand, chr, pos), seq))
         r = f.readline()
     f.close()
     ftab.close()
     fbed.close()
+    ffasta.close()
     os.remove(polyadb_temp)
 
 def annotate_pair(species, chr, strand, pos1, pos2):
@@ -178,7 +190,7 @@ def annotate_pair(species, chr, strand, pos1, pos2):
         pos1, pos2 = pos2, pos1
     _, gid1, _, interval1 = annotate_position(species, chr, strand, pos1)
     _, gid2, _, interval2 = annotate_position(species, chr, strand, pos2)
-    
+
     assert(gid1==gid2) # pair needs to be in the same gene
     # intervals are always in the positive orientation: index0, index1, index2...regardless of strand
     # example: - strand: [[100,200,'o']=3'UTR, [200,300,'i'], [300,400,'o']=5'UTR]
