b9e4c718afe1ac9b4642464f581705e0bc343508
Gregor Rot <gregor.rot@gmail.com>
2015-12-10 14:12:05 +0100

    Changes in mapping, added sege mapper.

--- map/__init__.py
+++ map/__init__.py
@@ -5,18 +5,15 @@ import pybio
 import shutil
 import commands
 
-def map_experiment(lib_id, exp_id, map_id = 1, force=False):
+def map_experiment(lib_id, exp_id, map_id = 1, force=False, mapper="star", cpu=1):
     exp_id = int(exp_id)
     exp_data = apa.annotation.libs[lib_id].experiments[exp_id]
     map_folder = apa.path.map_folder(lib_id, exp_id, map_id = map_id)
     fastq_file = apa.path.map_fastq_file(lib_id, exp_id)
-    #if exp_data["map_to"] not in ["hg19", "mm10", "dm5", "rn5"]:
-    #    continue
     if os.path.exists(map_folder) and force==False:
         print "%s_e%s : MAP : skip (already mapped) or currently mapping" % (lib_id, exp_id)
         return
     if os.path.exists(map_folder):
-        # remove folder
         if map_folder.find(lib_id)>0 and map_folder.find("m%s" % map_id)>0 and len(lib_id)>6: # security
             shutil.rmtree(map_folder)
     try:
@@ -25,7 +22,10 @@ def map_experiment(lib_id, exp_id, map_id = 1, force=False):
         print "error creating mapping folder:", map_folder
         return False
     print "%s_e%s : MAP : %s" % (lib_id, exp_id, map_folder)
-    pybio.map.star(exp_data["map_to"], fastq_file, map_folder, "%s_e%s_m%s" % (lib_id, exp_id, map_id))
+    if mapper=="star":
+        pybio.map.star(exp_data["map_to"], fastq_file, map_folder, "%s_e%s_m%s" % (lib_id, exp_id, map_id), cpu=cpu)
+    if mapper=="sege":
+        pybio.map.sege(exp_data["map_to"], fastq_file, map_folder, "%s_e%s_m%s" % (lib_id, exp_id, map_id), cpu=cpu)
 
 def stats(lib_id):
     fname = os.path.join(apa.path.lib_folder(lib_id), "%s_stats.tab" % lib_id)
