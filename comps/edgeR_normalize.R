8ad9c554c06b75b3e6107812bfe431beafb93e38
Gregor Rot <gregor.rot@gmail.com>
2017-09-26 07:51:10 +0200

    Towards expressRNA version 1.1 with code refactoring, improved novel plots and more structured result reporting.

---
+++ comps/edgeR_normalize.R
@@ -0,0 +1,16 @@
+library("edgeR")
+library("data.table")
+
+args = commandArgs(trailingOnly = T); # trailingOnly: only take parameters after --args
+input_fname = args[1]
+output_fname = args[2]
+
+gx <- fread(input_fname, colClasses=list(character=1:1))
+gxcounts <- gx[,-c(1:9),with=FALSE]
+d <- DGEList(counts=gxcounts)
+d <- calcNormFactors(d)
+
+d$genes <- gx[,1:8,with=FALSE]
+d$genes <- cbind(d$genes, round(cpm(d), 1))
+
+write.table(d$genes, file=output_fname, sep="\t", row.names=FALSE, quote=FALSE)
