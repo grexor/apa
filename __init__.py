f61ec28e69eeb47d8d9ef8c0c752fb3e1836d6b8
Gregor Rot <gregor.rot@gmail.com>
2021-02-09 11:20:10 +0100

    Config fix

--- __init__.py
+++ __init__.py
@@ -24,8 +24,8 @@ import apa.motifs
 version = "1.3"
 
 apa.path.init() # inicialize paths
-apa.annotation.init() # read annotations
 apa.config.init()
+apa.annotation.init() # read annotations
 
 def get_read_len(lib_id, exp_id):
     f = pybio.data.Fastq(apa.path.map_fastq_file(lib_id, exp_id))
