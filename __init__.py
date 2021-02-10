b50f8a85bb874f05fbe07048196e3191ade15449
Gregor Rot <gregor.rot@gmail.com>
2021-02-10 10:25:37 +0100

    Fixed some path issues

--- __init__.py
+++ __init__.py
@@ -31,6 +31,7 @@ apa.path.init() # inicialize paths
 apa.annotation.init() # read annotations
 
 def get_read_len(lib_id, exp_id):
+    print(apa.path.map_fastq_file(lib_id, exp_id))
     f = pybio.data.Fastq(apa.path.map_fastq_file(lib_id, exp_id))
     f.read()
     return len(f.sequence)
